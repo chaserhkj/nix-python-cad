9eba76307f6cf63389e4bc1a1936adbc4d3b8a4a
Kangjing Huang <huangkangjing@gmail.com>
2024-06-25 16:53:32 -0400

    Sanitize version string for build123d

--- patched/lib3mf/default.nix
+++ patched/lib3mf/default.nix
@@ -1,4 +1,5 @@
-{ lib3mf }: let _fetched = (callPackage ../_sources/generated.nix {}).lib3mf;
+{ lib, lib3mf, callPackage }:
+let _fetched = (callPackage ../../_sources/generated.nix {}).lib3mf;
 in lib3mf.overrideAttrs {
     inherit (_fetched) src;
     version = lib.strings.removePrefix "v" _fetched.version;
