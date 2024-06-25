8a492dbadfe1959a74adcd1c7c27dc1c450bc745
Kangjing Huang <huangkangjing@gmail.com>
2024-06-25 15:02:37 -0400

    Add packages module

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Description for the project";
+  description = "Nix packaging of python CAD packages";
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -9,27 +9,9 @@
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
-
+        ./packages/module.nix
       ];
       # Only x86_64-linux is tested at the moment, will try to add more later
       systems = [ "x86_64-linux" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.hello;
-      };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-
-      };
     };
 }
