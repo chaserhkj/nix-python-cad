74736e2ab86e3b7ac2b5946e8483495609713b72
Kangjing Huang <huangkangjing@gmail.com>
2024-06-26 16:23:35 -0400

    Use _sources

--- patched/default.nix
+++ patched/default.nix
@@ -2,8 +2,9 @@
   perSystem = {pkgs, self', ...}: {
     packages = with pkgs;
       let localPkgs = self'.packages;
+      _src = self'._sources;
     in rec {
-      lib3mf = callPackage ./lib3mf {};
+      lib3mf = callPackage ./lib3mf {inherit _src;};
     };
   };
 }
